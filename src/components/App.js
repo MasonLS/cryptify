20880719cd3ec2f1ec44226c9747c78e16301216
Mason Seale <masonseale88@gmail.com>
2017-01-13 18:44:56 -0500

    minor config

---
+++ src/components/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedTrack: null,
+      selectedArtist: null
+    }
+  }
+  
+  render() {
+    return (
+      <Grid>
+        <Row>
+          <Col sm={6}>
+          </Col>
+          <Col sm={6}>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={12}>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={12}>
+          </Col>
+        </Row>
+      </Grid>
+    );
+  }
+}
+
+export default App;
