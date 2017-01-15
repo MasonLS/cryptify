0c671ac6c2199ad679cb5723a909e2ed8780c232
Mason Seale <masonseale88@gmail.com>
2017-01-15 13:54:42 -0500

    artists and tracks routes

--- src/components/login.js
+++ src/components/login.js
@@ -13,7 +13,7 @@ class Login extends Component {
             <h3>
               Please log in to your Spotify account to get started.
             </h3>
-            <Button href="https://accounts.spotify.com/authorize?client_id=49859018c9a4462cbb4336259546f1e9&redirect_uri=http%3A%2F%2Flocalhost%3A3001%2Fauth%2Fcallback&response_type=code&scope=user-top-read">Log in</Button>
+            <Button href="https://accounts.spotify.com/authorize?client_id=49859018c9a4462cbb4336259546f1e9&redirect_uri=http%3A%2F%2Flocalhost%3A3001%2Fauth%2Fcallback&response_type=code&scope=user-top-read%20user-read-private">Log in</Button>
           </Col>
         </Row>
       </Grid>
