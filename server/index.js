9b970f9d24f8fa35a68195bac36fc36ed2a4ccd2
Mason Seale <masonseale88@gmail.com>
2017-01-17 15:28:07 -0500

    heroku stuff

--- server/index.js
+++ server/index.js
@@ -50,7 +50,7 @@ app.use('/tracks/', tracks);
 
 if (env.NODE_ENV === 'production') {
   app.use('/*', (req, res) => {
-    res.send(path.join(__dirname, '../build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
   });
 }
 
